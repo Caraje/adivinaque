39ab4bec6e42235559c244b1b4813a1edc1275ec
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-31 18:11:56 +0200

    create: loginButtonComponent

---
+++ src/components/ui/LoginButton.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const LoginButton = () => {
+  return (
+    <Link
+      className='flex items-center gap-4 hover:text-adivinaGreen hover:shadow-adivinaGreen '
+      href='/auth/login'
+    >
+      <h2 className='font-semibold text-lg'>Login</h2>
+      <img src='./icons/user-icon.svg' alt='Icono de usuario' width={30} height={30} />
+    </Link>
+  )
+}
+
+export default LoginButton
