5413ee4c15a82a4dfa705b37bd895b6a7d8707e9
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-31 18:12:18 +0200

    create: userAutenticated component

---
+++ src/components/ui/UserAutenticated.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const UserAutenticated = ({ user }) => {
+  return (
+    <Link href='/auth/login' className='flex items-center gap-4 hover:text-adivinaGreen hover:shadow-adivinaGreen '>
+      <h5 className='font-semibold text-lg' href='/caraje'>{user.userName}</h5>
+      <img className='rounded-full' src={user.avatar || './imgs/no-avatar.webp'} alt='Imagen de avatar de Caraje' width={40} height={40} />
+    </Link>
+  )
+}
+
+export default UserAutenticated
