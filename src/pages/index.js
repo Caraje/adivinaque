10bc1d11b3c3ffe3afdb84df209d3b245bd4cb60
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-31 17:20:14 +0200

    add: SelectCategory components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 
 import NewsCard from '@/components/home/NewsCard'
+import SelectCategory from '@/components/home/SelectCategory'
 import CategoryCard from '@/components/ui/CategoryCard'
 import UserCard from '@/components/ui/UserCard'
 import Link from 'next/link'
@@ -30,40 +31,7 @@ export default function Home () {
 
         {/* CONTENIDO */}
         <main className='w-full flex gap-10 p-4 max-w-6xl'>
-
-          <section className=' w-full flex flex-col gap-8  border border-adivinaGreen/50 rounded-xl p-8 bg-adivinaBlack/25'>
-            <h1 className='font-extrabold text-3xl text-adivinaGreen'>
-              Selecciona una categoria
-            </h1>
-            <div className='flex flex-col gap-8 justify-center items-center '>
-
-              <div className='flex justify-center max-w-2xl w-full'>
-
-                <CategoryCard
-                  category='cinema'
-                  name='Cine'
-                  image='./imgs/cinema.webp'
-                  wide
-                />
-              </div>
-
-              <div className='flex justify-between max-w-2xl w-full'>
-                <CategoryCard
-                  category='series'
-                  name='Series'
-                  image='./imgs/series.webp'
-                  wide={false}
-                />
-                <CategoryCard
-                  category='videogames'
-                  name='Juegos'
-                  image='./imgs/game.webp'
-                  wide={false}
-                />
-
-              </div>
-            </div>
-          </section>
+          <SelectCategory />
           <aside className='w-80 flex flex-col gap-10  '>
             {/* USER CARD */}
             <UserCard />
