9396b6ca7f9090724c6eaa693a5a8af26e2299c3
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-01 16:38:32 +0200

    fix url for images

--- src/components/ui/UserAutenticated.jsx
+++ src/components/ui/UserAutenticated.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 const UserAutenticated = ({ user }) => {
   return (
     <Link href='/auth/login' className='flex items-center gap-4 hover:text-adivinaGreen hover:shadow-adivinaGreen '>
-      <h5 className='font-semibold text-lg' href='/caraje'>{user.userName}</h5>
-      <img className='rounded-full' src={user.avatar || './imgs/no-avatar.webp'} alt='Imagen de avatar de Caraje' width={40} height={40} />
+      <h5 className='font-semibold text-lg' href={`/users/${user.userName}`}>{user.userName}</h5>
+      <img className='rounded-full' src={user.avatar || '/imgs/no-avatar.webp'} alt='Imagen de avatar de Caraje' width={40} height={40} />
     </Link>
   )
 }
