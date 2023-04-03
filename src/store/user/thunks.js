8ffb9008f9cc3606252fdbb03c1df0a582d82be6
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-03 18:37:42 +0200

    add: userUpdate to Thunk userSlice

--- src/store/user/thunks.js
+++ src/store/user/thunks.js
@@ -1,7 +1,12 @@
-import { getUser } from './userSlice'
+import { getUser, updateUser } from './userSlice'
 
 export const userState = (user) => {
   return async (dispatch) => {
     dispatch(getUser(user))
+  }
+}
+export const userUpdateState = (dataUser) => {
+  return async (dispatch) => {
+    dispatch(updateUser(dataUser))
   }
 }
