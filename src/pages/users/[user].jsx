c2dbe9f7681ca6b2b43a5b27607e0e72f68dd252
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-03 13:06:36 +0200

    add: userCardSocials and formUpdateData

--- src/pages/users/[user].jsx
+++ src/pages/users/[user].jsx
@@ -1,104 +1,55 @@
 import MainLayout from '@/components/layout/MainLayout'
-import CategoryCard from '@/components/ui/CategoryCard'
+import CategoryCardUser from '@/components/user/CategoryCardUser'
 import { UserCardSocial } from '@/components/user/UserCardSocial'
+import FormUpdateSocial from '@/components/user/formUpdateSocial'
 import { getUserList } from '@/services/supabase'
+import { useSelector } from 'react-redux'
 
 const UserPage = ({ user }) => {
+  const { id } = useSelector(state => state.auth)
+  const userId = user[0].id
   const actualUser = user[0].user_metadata
+  const category = Object.keys(actualUser.categories)
 
   return (
     <MainLayout>
       <div className='flex flex-col w-full gap-10 p-4 max-w-6xl '>
         <div className='flex w-full gap-8 '>
           <section className='w-2/6 max-w-xl '>
-            <UserCardSocial social={actualUser.socials} />
+            <UserCardSocial social={actualUser.socials} user={actualUser} />
 
           </section>
           <section className='w-full '>
-            <article className='flex  flex-col gap-4 justify-center items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
-              <form
-                className='w-full grid grid-cols-2 gap-4'
-              >
-                <div className='flex flex-col gap-4'>
-                  User name
-                  <input
-                    className='w-full rounded-xl bg-transparent border border-[#03fea4] p-2'
-                    type='text'
 
-                  />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  Sube imagen
-                  <input
-                    type='file'
-
-                  />
-
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  Twitter
-                  <input
-                    className='w-full rounded-xl bg-transparent border border-[#03fea4] p-2'
-                    type='text'
-
-                  />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  instagram
-                  <input
-                    className='w-full rounded-xl bg-transparent border border-[#03fea4] p-2'
-                    type='text'
-
-                  />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  twitch
-                  <input
-                    className='w-full rounded-xl bg-transparent border border-[#03fea4] p-2'
-                    type='text'
-
-                  />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  web
-                  <input
-                    className='w-full rounded-xl bg-transparent border border-[#03fea4] p-2'
-                    type='text'
-                    flex-col
-                  />
-                </div>
-
-                <button className='w-fit px-8 justify-self-end  rounded-xl bg-[#03fea4] text-[#333] '>Actualizar</button>
-              </form>
-            </article>
+            {
+              (id === userId) &&
+                <article className='flex  flex-col gap-4 justify-center items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
+                  <FormUpdateSocial />
+                </article>
+            }
           </section>
         </div>
         <section className='w-full flex gap-10 max-w-6xl'>
-          <article className=' w-full flex  justify-evenly items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
-
-            <CategoryCard
-              category='cinema'
+          <article className=' w-full flex  gap-8 justify-evenly items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
+            <CategoryCardUser
+              category={category[0]}
               name='Cine'
               image='/imgs/cinema.webp'
-              wide={false}
+              score={actualUser.categories.cinema}
             />
-            <CategoryCard
-              category='series'
+            <CategoryCardUser
+              category={category[1]}
               name='Series'
               image='/imgs/series.webp'
-              wide={false}
+              score={actualUser.categories.series}
             />
-            <CategoryCard
-              category='videogames'
+            <CategoryCardUser
+              category={category[2]}
               name='Juegos'
               image='/imgs/game.webp'
-              wide={false}
+              score={actualUser.categories.videogames}
             />
+
           </article>
         </section>
       </div>
