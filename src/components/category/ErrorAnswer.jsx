9709e7dac889b9f1f1d733760b185c50d272be25
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-04 14:15:00 +0200

    create: error component

---
+++ src/components/category/ErrorAnswer.jsx
@@ -0,0 +1,12 @@
+
+const ErrorAnswer = () => {
+  return (
+    <h3
+      className='absolute bottom-4 w-full bg-red-700/40 ext-adivinaBlack font-bold  flex items-center justify-center text-3xl p-4'
+    >
+      ¡¡¡Respuesta Incorrecta!!!
+    </h3>
+  )
+}
+
+export default ErrorAnswer
