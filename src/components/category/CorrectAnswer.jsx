5abf475b28fd2af35e3bdbf0ea91e57879131819
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-04 14:14:47 +0200

    create CorrectAnswer component

---
+++ src/components/category/CorrectAnswer.jsx
@@ -0,0 +1,12 @@
+
+const CorrectAnswer = () => {
+  return (
+    <h3
+      className='absolute bottom-4 w-full bg-adivinaGreen/40 ext-adivinaBlack font-bold  flex items-center justify-center text-3xl p-4'
+    >
+      ¡¡¡Respuesta Correcta!!!
+    </h3>
+  )
+}
+
+export default CorrectAnswer
