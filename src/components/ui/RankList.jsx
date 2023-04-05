9156f49251d675a888232a4965ccb9eaefe56f9d
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-05 13:13:12 +0200

    create: RankList Component

---
+++ src/components/ui/RankList.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+const RankList = ({ usersList }) => {
+  return (
+    <div className='h-60 overflow-x-hidden scrollbar scrollbar-track-rounded-xl scrollbar-w-[3px] scrollbar-thumb-[#03fea4] scrollbar-track-[#333] '>
+      {usersList.slice(0, 10).map((user, idx) => {
+        const newUser = user.user_metadata
+
+        return (
+          <Link
+            href={`/users/${newUser.userName.toLowerCase().trim()}`}
+            target='_blank'
+            key={idx}
+            className='w-full h-12 flex justify-between items-center gap- p-2 rounded-xl hover:bg-adivinaGreen/25 hover:scale-105 transition-all'
+          >
+            <img
+              className='rounded-full w-8 h-8 object-cover border border-adivinaGreen'
+              src={newUser.imgAvatar
+                ? `https://res.cloudinary.com/caraje/image/upload/v1679717935/${newUser.imgAvatar}`
+                : '/imgs/no-avatar.webp'} alt={`Imagen de ${newUser.userName}`} width={50} height={50}
+            />
+            <h2 className='w-3/5 font-montserrat font-semibold'>{newUser.userName}</h2>
+            <h2 className='pr-2'>{newUser.categories.cinema.totalPoints}</h2>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default RankList
