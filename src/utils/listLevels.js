470d0e627b90fe393c3963cc1c3ee6447d5d5f45
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-06 17:16:25 +0200

    create: function for get and filter level list

---
+++ src/utils/listLevels.js
@@ -0,0 +1,23 @@
+import cinema from '@/db/levelsCategories/cinema.json'
+
+const date = new Date()
+export const actualDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+// Get Actual Day
+export const getLevelOfDay = () => {
+  const LevelDay = cinema.cinema.filter(lvl => {
+    return lvl.publishDay === actualDate
+  })
+  return LevelDay
+}
+
+// Get filtered List of Valid Levels
+export const getLevelsList = (scoreUser) => {
+  const levelsList = cinema.cinema
+    .filter(lvl => lvl.id <= getLevelOfDay()[0].id)
+    .filter(lvl => !scoreUser.cinema?.levels_completed
+      .includes(lvl.id))
+    .sort((a, b) => b.id - a.id)
+
+  return levelsList
+}
