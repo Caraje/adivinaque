84ce4b9a8b21e6f187633300216ccf242c4e21e4
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-10 13:00:33 +0200

    create: logoutState thunk

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,7 +1,15 @@
-import { login } from './authSlice'
+import { logoutUser } from '@/services/supabase'
+import { login, logout } from './authSlice'
 
 export const loginState = (user) => {
   return async (dispatch) => {
     dispatch(login(user))
+  }
+}
+
+export const logoutState = () => {
+  logoutUser()
+  return async (dispatch) => {
+    dispatch(logout())
   }
 }
