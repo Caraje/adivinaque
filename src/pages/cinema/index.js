394f11f36e65847b65e8251f0394babba13b96d6
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-10 12:07:41 +0200

    add:conditional component no levels and fix reset value form

--- src/pages/cinema/index.js
+++ src/pages/cinema/index.js
@@ -5,12 +5,13 @@ import { useState, useEffect } from 'react'
 import CanvasCategory from '@/components/category/CanvasCategory'
 import AnswerForm from '@/components/category/AnswerForm'
 import CluesCategory from '@/components/category/CluesCategory'
-import { getUserList } from '@/services/supabase'
+import { getUserList, logoutUser } from '@/services/supabase'
 import RankList from '@/components/ui/RankList'
 // import { getLevelsList } from '@/utils/listLevels'
 import { getPositionUserRank } from '@/utils/users'
 import { useUpdateScoreUser } from '@/hooks/useUpdateScoreUser'
 import { useScoreGame } from '@/hooks/useScoreGame'
+import NoLevels from '@/components/category/NoLevels'
 
 export default function CinemaPage ({ usersList }) {
   const [formAnswer, setFormAnswer] = useState('')
@@ -26,6 +27,7 @@ export default function CinemaPage ({ usersList }) {
 
   useEffect(() => {
     resetScoreLevel()
+    setFormAnswer('')
   }, [actualLevel])
   const handleAnswer = (event) => {
     event.preventDefault()
@@ -47,15 +49,18 @@ export default function CinemaPage ({ usersList }) {
 
           {/* CONTENIDO */}
           <main className='w-full flex gap-10 p-4 max-w-6xl'>
-            <section className=' w-full flex flex-col gap-8  border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
+            <section className='relative w-full flex flex-col gap-8  border border-adivinaGreen/50 rounded-xl p-4 bg-adivinaBlack/25'>
               <h1 className='font-black text-3xl text-adivinaGreen ml-8 mt-8'>
                 Cine
               </h1>
               {
                 (level)
                   ? (
-                    <div className=' w-full flex flex-col gap-8  items-center overflow-hidden'>
+                    <div className='w-full flex flex-col gap-4  items-center overflow-hidden'>
                       <CanvasCategory level={level} isCorrect={isCorrect} isError={isError} turn={turn} />
+                      <div className='w-4/5  font-montserrat font-semibold text-adivinaGreen text-xs'>
+                        <span className='text-white text-base font-normal'>Dia: </span>{level.publishDay}
+                      </div>
                       <AnswerForm
                         handleAnswer={handleAnswer}
                         formAnswer={formAnswer}
@@ -70,9 +75,9 @@ export default function CinemaPage ({ usersList }) {
                       {turn >= 1 && <CluesCategory level={level} turn={turn} />}
                     </div>
                     )
-                  : <h1>No hay mas niveles</h1> // TODO: Hay que hacer un mensjae de aviso de no mas niveles
-              }
-
+                  : <NoLevels />
+                  }
+              <p className=' absolute bottom-1 w-4/5 font-thin '>* Se debe introducir el titulo en su version original</p>
             </section>
             <aside className='w-80 flex flex-col gap-10  '>
               {status && <UserCard />}
@@ -84,7 +89,10 @@ export default function CinemaPage ({ usersList }) {
               </section>
             </aside>
           </main>
-          <footer className='w-full mt-auto flex items-center  justify-around bg-adivinaBlack'>Aqui va el pie de pagina</footer>
+          <footer className='w-full mt-auto flex items-center  justify-around bg-adivinaBlack'>
+            <button onClick={() => { logoutUser() }}>X</button>
+            Aqui va el pie de pagina
+          </footer>
         </MainLayout>
       </div>
     </>
