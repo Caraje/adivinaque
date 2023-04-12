ddaa2d0b2d935609f4542ec7cf7b5e9a51120cc0
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-12 20:27:10 +0200

    add resetScore

--- src/hooks/useScoreGame.js
+++ src/hooks/useScoreGame.js
@@ -1,11 +1,8 @@
 
 import { useState } from 'react'
-import { useUpdateScoreUser } from './useUpdateScoreUser'
 import { getLevelsList } from '@/utils/listLevels'
 
 export const useScoreGame = (categoryDB, scoreUser, userPosition) => {
-  const { setTotalPoints, setErrorsCount, setCorrects } = useUpdateScoreUser(categoryDB, scoreUser, userPosition)
-
   const [actualLevel, setActualLevel] = useState(0) // Estado con el level actual
   const [turn, setTurn] = useState(0) // estado con el turno del nivel actual
   const [isCorrect, setIsCorrect] = useState(false) // establece si la respuesta es correcta o no
@@ -15,32 +12,33 @@ export const useScoreGame = (categoryDB, scoreUser, userPosition) => {
   const level = getLevelsList(categoryDB)[levelNumber]
   const levelList = getLevelsList(categoryDB)
 
-  const isAnswerCorrect = (setTotalPoints, setCorrects, corrects) => {
+  const isAnswerCorrect = async (setTotalPoints, setCorrects, corrects) => {
     setIsCorrect(true)
     setTotalPoints(5 * multiplyPoints)
     setCorrects(corrects + 1)
     levelNumber = levelNumber + 1
   }
 
-  const isAnswerIncorrect = (setErrorsCount, errorsCount) => {
+  const isAnswerIncorrect = async (setErrorsCount, errorsCount) => {
     setTurn(turn + 1)
     setMultiplyPoints(multiplyPoints - 1)
     setErrorsCount(errorsCount + 1)
   }
 
-  const isAnswerFail = (setErrorsCount, errorsCount) => {
+  const isAnswerFail = async (setErrorsCount, errorsCount) => {
     setIsError(true)
     setErrorsCount(errorsCount + 1)
     levelNumber = levelNumber + 1
   }
 
-  const resetScoreLevel = () => {
+  const resetScore = async (setTotalPoints, setErrorsCount, setCorrects, setFormAnswer) => {
     setTurn(0)
     setIsError(false)
     setIsCorrect(false)
     setTotalPoints(0)
     setErrorsCount(0)
     setCorrects(0)
+    setFormAnswer('')
   }
 
   return {
@@ -57,10 +55,8 @@ export const useScoreGame = (categoryDB, scoreUser, userPosition) => {
     isAnswerCorrect,
     isAnswerIncorrect,
     isAnswerFail,
-    resetScoreLevel,
     level,
     levelList,
-    levelNumber
-
+    resetScore
   }
 }
