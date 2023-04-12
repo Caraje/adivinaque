5057343ea397635499133d82be020559cb839066
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-12 13:53:08 +0200

    fix: rankList component

--- src/components/ui/RankList.jsx
+++ src/components/ui/RankList.jsx
@@ -1,15 +1,20 @@
+import { getOrderedRank } from '@/utils/users'
 import Link from 'next/link'
 import React from 'react'
 
-const RankList = ({ usersList }) => {
-  const orderList = usersList.sort((a, b) => {
-    return (b.user_metadata.categories.cinema.totalPoints - a.user_metadata.categories.cinema.totalPoints)
-  })
+const RankList = ({ categoryDB, usersList }) => {
+  const orderList = getOrderedRank(categoryDB, usersList)
 
+  // console.log({ categoryDB })
   return (
     <div className='h-60 overflow-x-hidden scrollbar scrollbar-track-rounded-xl scrollbar-w-[3px] scrollbar-thumb-[#03fea4] scrollbar-track-[#333] '>
       {orderList.slice(0, 10).map((user, idx) => {
         const newUser = user.user_metadata
+        const points = (categoryDB.category === 'cinema')
+          ? (newUser.categories.cinema.totalPoints)
+          : (categoryDB.category === 'series')
+              ? (newUser.categories.series.totalPoints)
+              : (newUser.categories.videogames.totalPoints)
 
         return (
           <Link
@@ -25,7 +30,7 @@ const RankList = ({ usersList }) => {
                 : '/imgs/no-avatar.webp'} alt={`Imagen de ${newUser.userName}`} width={50} height={50}
             />
             <h2 className='w-3/5 font-montserrat font-semibold'>{newUser.userName}</h2>
-            <h2 className='pr-2'>{newUser.categories.cinema.totalPoints}</h2>
+            <h2 className='pr-2'>{points}</h2>
           </Link>
         )
       })}
