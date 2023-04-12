be8037c050cdc5556b9acdb07d4bdcc9c9cebe24
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-12 13:52:01 +0200

    refactor getLevelDay and getLevelList

--- src/utils/listLevels.js
+++ src/utils/listLevels.js
@@ -1,21 +1,21 @@
-import cinema from '@/db/levelsCategories/cinema.json'
 
 const date = new Date()
 export const actualDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
 // Get Actual Day
-export const getLevelOfDay = () => {
-  const LevelDay = cinema.cinema.filter(lvl => {
+export const getLevelOfDay = (categoryDataBase) => {
+  const LevelDay = categoryDataBase.filter(lvl => {
     return lvl.publishDay === actualDate
   })
   return LevelDay
 }
 
 // Get filtered List of Valid Levels
-export const getLevelsList = (scoreUser) => {
-  const levelsList = cinema.cinema
-    .filter(lvl => lvl.id <= getLevelOfDay()[0]?.id)
-    .filter(lvl => !scoreUser?.cinema?.levels_completed
+export const getLevelsList = (categoryDB) => {
+  const { categoryDataBase, userScore } = categoryDB
+  const levelsList = categoryDataBase
+    .filter(lvl => lvl.id <= getLevelOfDay(categoryDataBase)[0]?.id)
+    .filter(lvl => !userScore?.levels_completed
       .includes(lvl.id))
     .sort((a, b) => b.id - a.id)
 
