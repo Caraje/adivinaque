f7a6c9c1765102dc7ebfadf7e79da87bcce1dde4
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-12 20:26:56 +0200

    fix: count points and reload state

--- src/components/category/AnswerForm.jsx
+++ src/components/category/AnswerForm.jsx
@@ -3,7 +3,23 @@ import { userScoreState } from '@/store/user/thunks'
 import { previusLevel } from '@/utils/listLevels'
 import { useDispatch } from 'react-redux'
 
-const AnswerForm = ({ handleAnswer, formAnswer, setFormAnswer, isCorrect, isError, actualLevel, setActualLevel, levelList, pointsUser, status, setIsAutenticated }) => {
+const AnswerForm = ({
+  handleAnswer,
+  formAnswer,
+  setFormAnswer,
+  isCorrect,
+  isError,
+  actualLevel,
+  setActualLevel,
+  levelList,
+  pointsUser,
+  status,
+  setIsAutenticated,
+  resetScore,
+  setTotalPoints,
+  setErrorsCount,
+  setCorrects
+}) => {
   const dispatch = useDispatch()
   const handleResetLevel = async () => {
     if (status !== 'authenticated') {
@@ -11,14 +27,18 @@ const AnswerForm = ({ handleAnswer, formAnswer, setFormAnswer, isCorrect, isErro
       setIsAutenticated(true)
       return
     }
-    await dispatch(userScoreState(pointsUser))
     previusLevel(setActualLevel, actualLevel, levelList)
+    dispatch(userScoreState(pointsUser))
+    setTimeout(() => {
+      resetScore(setTotalPoints, setErrorsCount, setCorrects, setFormAnswer)
+    }, 10)
   }
   return (
     <form
       className='w-4/5 flex gap-4 h-12'
       onSubmit={handleAnswer}
     >
+      {JSON.stringify(isCorrect)}
       <input
         className='w-4/5 rounded-lg p-4 bg-adivinaBlack/25 border border-adivinaGreen'
         type='text'
@@ -29,23 +49,23 @@ const AnswerForm = ({ handleAnswer, formAnswer, setFormAnswer, isCorrect, isErro
       />
 
       {
-                    (isCorrect || isError)
-                      ? (
-                        <button
-                          type='button'
-                          onClick={handleResetLevel}
-                          className='w-1/5 bg-yellow-400 border border-adivinaGreen text-adivinaBlack font-bold rounded-lg hover:scale-105 hover:contrast-200 transition-all '
-                        >
-                          Nivel anterior
-                        </button>
-                        )
-                      : (
-                        <button
-                          className='w-1/5 bg-adivinaGreen border border-adivinaGreen text-adivinaBlack font-bold rounded-lg hover:scale-105 hover:contrast-200 transition-all '
-                        >
-                          Enviar
-                        </button>
-                        )
+        (isCorrect || isError)
+          ? (
+            <button
+              type='button'
+              onClick={handleResetLevel}
+              className='w-1/5 bg-yellow-400 border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
+            >
+              Nivel anterior
+            </button>
+            )
+          : (
+            <button
+              className='w-1/5 bg-adivinaGreen border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
+            >
+              Enviar
+            </button>
+            )
                   }
     </form>
   )
