7ffe876dac48d539eef9b025f11429c523636bf5
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-13 13:38:46 +0200

    change icons

--- src/components/ui/LoginButton.jsx
+++ src/components/ui/LoginButton.jsx
@@ -1,13 +1,17 @@
+import { loginIcon } from '@/utils/icons'
 import Link from 'next/link'
 
 const LoginButton = () => {
   return (
     <Link
-      className='flex items-center gap-4 hover:text-adivinaGreen hover:shadow-adivinaGreen '
+      className='flex items-center justify-center gap-2 hover:text-adivinaGreen hover:shadow-adivinaGreen '
       href='/auth/login'
     >
       <h2 className='font-semibold text-lg'>Login</h2>
-      <img src='/icons/user-icon.svg' alt='Icono de usuario' width={30} height={30} />
+      <picture className=''>
+        {loginIcon}
+      </picture>
+      {/* <img src='/icons/user-icon.svg' alt='Icono de usuario' width={30} height={30} /> */}
     </Link>
   )
 }
