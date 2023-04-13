fa0e2219646baf3372d27b8773a2c9d433d51e09
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-13 13:37:59 +0200

    change: icons

--- src/components/user/UserCardSocial.jsx
+++ src/components/user/UserCardSocial.jsx
@@ -1,12 +1,23 @@
+import { instagramIcon, twitchIcon, twitterIcon, webIcon, youtubeIcon } from '@/utils/icons'
+import Link from 'next/link'
 
 export const UserCardSocial = ({ user }) => {
   const validSocials = user.socials.filter(soc => soc.user !== '')
 
+  const SOCIAL_NETWORKS = {
+    twitter: twitterIcon,
+    instagram: instagramIcon,
+    twitch: twitchIcon,
+    youtube: youtubeIcon,
+    web: webIcon
+  }
+
+  console.log({ validSocials })
   return (
-    <article className=' flex  flex-col gap-4 justify-center items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-8 bg-adivinaBlack/25'>
+    <article className='h-full flex  flex-col gap-4 justify-center items-center overflow-hidden border border-adivinaGreen/50 rounded-xl p-8 bg-adivinaBlack/25'>
       <header className='flex flex-col justify-center items-center gap-2'>
         <img
-          className='rounded-full w-[10rem] h-[10rem] object-cover border-4 border-adivinaGreen'
+          className='rounded-full w-[7rem] h-[7rem] object-cover border-2 border-adivinaGreen'
           src={user.imgAvatar ? `https://res.cloudinary.com/caraje/image/upload/v1679717935/${user.imgAvatar}` : '/imgs/no-avatar.webp'}
           alt='Imagen del avatar de Caraje'
           width={125}
@@ -19,18 +30,26 @@ export const UserCardSocial = ({ user }) => {
       <footer>
         <div className='flex gap-2 flex-wrap justify-center items-center'>
           {
-            validSocials.map(soc => (
-              <a
-                className='hover:scale-125 transition-all hover:bg-adivinaGreen rounded-full p-1'
-                href={(soc.name === 'web') ? `${soc.url}` : `${soc.url}${soc.user}`}
-                target='_blank'
-                rel='noreferrer'
-                key={soc.name}
-              >
-                <img src={`/icons/${soc.name}-icon.svg`} alt={`Imagen del icono de ${soc.name}`} width={30} height={30} />
-              </a>
+            validSocials.map(soc => {
+              let icon
+              for (const [social, value] of Object.entries(SOCIAL_NETWORKS)) {
+                if (soc.name === social) {
+                  icon = value
+                }
+              }
+              return (
+                <Link
+                  className=' scale-75 hover:scale-125 transition-all hover:bg-adivinaGreen  hover:text-adivinaDark rounded-full p-1'
+                  href={(soc.name === 'web') ? `${soc.url}` : `${soc.url}${soc.user}`}
+                  target='_blank'
+                  rel='noreferrer'
+                  key={soc.name}
+                >
+                  {icon}
+                </Link>
 
-            ))
+              )
+            })
           }
         </div>
       </footer>
