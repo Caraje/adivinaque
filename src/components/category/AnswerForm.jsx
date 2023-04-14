10f65932c4ef6a8515eaf80d7d98d11e51e1e7de
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-14 13:31:00 +0200

    add: responsive Mode

--- src/components/category/AnswerForm.jsx
+++ src/components/category/AnswerForm.jsx
@@ -35,12 +35,11 @@ const AnswerForm = ({
   }
   return (
     <form
-      className='w-4/5 flex gap-4 h-12'
+      className='w-4/5 flex flex-col  sm:flex-row mb-4 gap-4 '
       onSubmit={handleAnswer}
     >
-      {JSON.stringify(isCorrect)}
       <input
-        className='w-4/5 rounded-lg p-4 bg-adivinaBlack/25 border border-adivinaGreen'
+        className='w-full sm:w-4/5 rounded-lg p-4 bg-adivinaBlack/25 border border-adivinaGreen'
         type='text'
         placeholder='Tu respuesta'
         name='answer'
@@ -54,14 +53,14 @@ const AnswerForm = ({
             <button
               type='button'
               onClick={handleResetLevel}
-              className='w-1/5 bg-yellow-400 border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
+              className='w-full sm:w-1/5 py-2 bg-yellow-400 border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
             >
               Nivel anterior
             </button>
             )
           : (
             <button
-              className='w-1/5 bg-adivinaGreen border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
+              className='w-full sm:w-1/5 py-2 bg-adivinaGreen border border-adivinaGreen text-adivinaBlack font-bold rounded-lg   hover:scale-105 hover:contrast-200 transition-all '
             >
               Enviar
             </button>
