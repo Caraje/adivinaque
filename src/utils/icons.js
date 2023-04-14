0ebdd20077603b95fa68b9888e897abb38fe49c5
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-14 13:30:43 +0200

    add: icon back

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -58,3 +58,11 @@ export const logoutIcon = (
 export const loginIcon = (
   <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-user-circle' width='40' height='40' viewBox='0 0 25 25' strokeWidth='1.5' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='12' cy='12' r='9' /><circle cx='12' cy='10' r='3' /><path d='M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855' /></svg>
 )
+
+export const backIcon = (
+  <svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-arrow-back-up' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1' />
+  </svg>
+
+)
