6826adfc973407948cb4f34de253dfb75cbce87e
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-14 13:31:18 +0200

    add: responsive Mode

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='w-full mt-auto flex items-center justify-around border-t-2 border-adivinaGreen '>
-      <div className='w-full flex gap-4 items-center justify-between max-w-6xl py-4'>
+    <footer className='w-full mt-auto flex items-center justify-around border-t-2 border-adivinaGreen'>
+      <div className='w-full flex flex-col sm:flex-row gap-4 items-center justify-between max-w-6xl py-4'>
         <div className='flex flex-col items-center gap-2'>
           <h2 className='font-extrabold text-lg text-adivinaGreen'>
             AdivinaQue ha sido creado por:
