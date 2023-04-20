eab5b6a0d7bc59d134c8aae36fec37caeee806fe
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-20 20:35:53 +0200

    add: vercel/analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import { store } from '@/store/store'
 import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react'
+
 import '@/styles/globals.css'
 import '../styles/atropos.css'
 
@@ -7,6 +9,7 @@ export default function App ({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   )
 }
