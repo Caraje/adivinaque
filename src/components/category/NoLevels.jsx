8e1162aa19eb84caf0f39d0d38fa8a8b79ff3f42
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-21 12:43:47 +0200

    Change images popCorn

--- src/components/category/NoLevels.jsx
+++ src/components/category/NoLevels.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 const NoLevels = () => {
   return (
     <div className='w-full h-full  flex items-center justify-center  '>
-      <img className='object-cover w-1/3' src='/imgs/popcorn.svg' alt='Illustration popcorn' width gap-8={250} height={300} />
+      <img className='object-cover w-1/3' src='https://res.cloudinary.com/caraje/image/upload/v1682073684/AdivinaQue/brn9uwtb48nyf25r0bzc.webp' alt='Illustration popcorn' width gap-8={250} height={300} />
       <div className='w-1/2 flex flex-col  items-center justify-center font-montserrat'>
         <h2 className='font-extrabold text-4xl text-center text-adivinaGreen mb-4'>¡¡¡Ya has completado todos los niveles!!!</h2>
         <h3 className=' text-base font-medium'>Vuelve mañana para poder ver nuevos niveles</h3>
--- src/components/category/NoUserAutenticated.jsx
+++ src/components/category/NoUserAutenticated.jsx
@@ -5,7 +5,7 @@ const NoUserAutenticated = () => {
   return (
     <div className=' absolute z-40 w-screen h-screen  flex items-center justify-center backdrop-blur-xl backdrop-brightness-50 '>
       <div className='w-1/2 h-1/2  flex items-center justify-center  '>
-        <img className='object-cover w-1/3' src='/imgs/sad-PopCorn.svg' alt='Illustration popcorn' width gap-8={250} height={300} />
+        <img className='object-cover w-1/3' src='https://res.cloudinary.com/caraje/image/upload/v1682073684/AdivinaQue/wq9zrs4vjekqdj2pxomz.webp' alt='Illustration popcorn' width gap-8={250} height={300} />
         <div className='w-1/2 flex flex-col  items-center justify-center font-montserrat'>
           <h2 className='font-extrabold text-5xl text-center text-adivinaGreen mb-4'>Oh, no estas registrado</h2>
           <h3 className=' text-base font-medium text-center w-[40ch]'>Si quieres, puedes acceder a más niveles registrandote o haciendo login con tu cuenta</h3>
