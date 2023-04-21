9694eebfeef24072c1cb569b1e45a299218e8b0e
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-21 18:56:40 +0200

    change img avatar empty

--- src/components/ui/UserAutenticated.jsx
+++ src/components/ui/UserAutenticated.jsx
@@ -10,7 +10,7 @@ const UserAutenticated = ({ user }) => {
         className='flex items-center justify-center gap-4 hover:text-adivinaGreen hover:shadow-adivinaGreen '
       >
         <h5 className='font-semibold text-lg' href={`/users/${user.userName}`}>{user.userName}</h5>
-        <img className='rounded-full' src={user.avatar ? `https://res.cloudinary.com/caraje/image/upload/v1679717935/${user.avatar}` : '/imgs/no-avatar.webp'} alt='Imagen de avatar de Caraje' width={40} height={40} />
+        <img className='rounded-full' src={user.avatar ? `https://res.cloudinary.com/caraje/image/upload/v1679717935/${user.avatar}` : 'https://res.cloudinary.com/caraje/image/upload/v1682092834/AdivinaQue/akxmwc0cpoffp1avtg8h.webp'} alt='Imagen de avatar de Caraje' width={40} height={40} />
       </button>
       {
         isHidden && <MenuUser user={user} setIsHidden={setIsHidden} />
