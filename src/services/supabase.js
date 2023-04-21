5e85339d42db2d7a2ec1f180a5ed375d83eb74ed
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-21 18:55:21 +0200

    change: image avatar default

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -14,7 +14,7 @@ export async function createUserWithEmail (userName, email, password) {
       options: {
         data: {
           userName,
-          imgAvatar: 'AdivinaQue/piwp3hgmtlq46v2uq5q2.webp',
+          imgAvatar: 'AdivinaQue/akxmwc0cpoffp1avtg8h.webp',
           url: `/${userName.toLowerCase().trim().replace(' ', '-')}`,
           isActive: true,
           socials: [
