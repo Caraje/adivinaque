bc52446966cb38ba9941ce10527d109ad6dad602
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-24 12:14:54 +0200

    add: categoryCardMobile

--- src/components/home/SelectCategory.jsx
+++ src/components/home/SelectCategory.jsx
@@ -1,7 +1,8 @@
 
 import CategoryCard from '../ui/CategoryCard'
+import CategoryCardMobile from '../ui/categoryCardMobile'
 
-const SelectCategory = () => {
+const SelectCategory = ({ isMobile }) => {
   return (
     <section className=' w-full flex flex-col gap-8  border border-adivinaGreen/50 rounded-xl p-8 bg-adivinaBlack/25'>
       <h1 className='font-extrabold text-3xl text-adivinaGreen'>
@@ -11,27 +12,48 @@ const SelectCategory = () => {
 
         <div className='flex justify-center max-w-2xl w-full'>
 
-          <CategoryCard
-            category='cinema'
-            name='Cine'
-            image='./imgs/cinema.webp'
-            wide
-          />
+          {!isMobile
+            ? <CategoryCard
+                category='cinema'
+                name='Cine'
+                image='./imgs/cinema.webp'
+                wide
+              />
+            : <CategoryCardMobile
+                category='cinema'
+                name='Cine'
+                image='./imgs/cinema.webp'
+                wide={false}
+              />}
         </div>
 
         <div className='flex flex-col sm:flex-row gap-8 justify-center sm:justify-between max-w-2xl w-full'>
-          <CategoryCard
-            category='series'
-            name='Series'
-            image='./imgs/series.webp'
-            wide={false}
-          />
-          <CategoryCard
-            category='videogames'
-            name='Juegos'
-            image='./imgs/game.webp'
-            wide={false}
-          />
+          {!isMobile
+            ? <CategoryCard
+                category='series'
+                name='Series'
+                image='./imgs/series.webp'
+                wide={false}
+              />
+            : <CategoryCardMobile
+                category='series'
+                name='Series'
+                image='./imgs/series.webp'
+                wide={false}
+              />}
+          {!isMobile
+            ? <CategoryCard
+                category='videogames'
+                name='Juegos'
+                image='./imgs/game.webp'
+                wide={false}
+              />
+            : <CategoryCardMobile
+                category='videogames'
+                name='Juegos'
+                image='./imgs/game.webp'
+                wide={false}
+              />}
 
         </div>
       </div>
