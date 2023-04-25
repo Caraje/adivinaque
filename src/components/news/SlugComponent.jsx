2f88a5868348237e75eb3a34d5c2bcb19786fb1a
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-25 12:33:10 +0200

    fix slugComponent

--- src/components/news/SlugComponent.jsx
+++ src/components/news/SlugComponent.jsx
@@ -1,9 +1,24 @@
 import { MDXRemote } from 'next-mdx-remote'
 import MDXComponents from './ComponentArticles/MDXComponents'
 
-const SlugComponent = ({ frontmatter, source, posts }) => {
+const SlugComponent = ({ frontmatter, source }) => {
+  const { title, img } = frontmatter
   return (
-    <MDXRemote {...source} components={MDXComponents} />
+    <>
+      <img
+        src={`https://res.cloudinary.com/caraje/image/upload/c_fill,g_faces,h_300,w_1000/f_webp/q_auto:eco/v1682415704/AdivinaQue/news/${img}`}
+        alt='cosas'
+        width={900}
+        height={500}
+        className='rounded-2xl '
+      />
+      <h1 className='font-black text-3xl text-adivinaGreen'>
+        {title}
+      </h1>
+      <div className='article_body font-montserrat '>
+        <MDXRemote {...source} components={MDXComponents} />
+      </div>
+    </>
   )
 }
 
