bb32bce9e9bde07dde4d6e91846a8fc9ea3c911c
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-25 12:27:49 +0200

    add styles for articles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/globals.css'
+import '@/styles/articles.css'
 import '../styles/atropos.css'
 
 export default function App ({ Component, pageProps }) {
