d3c2ec254e901c255a95fd3cf30256f7c864e6b2
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-04-27 11:43:58 +0200

    fix responsive no levels

--- src/components/category/NoLevels.jsx
+++ src/components/category/NoLevels.jsx
@@ -1,10 +1,10 @@
 
 const NoLevels = () => {
   return (
-    <div className='w-full h-full  flex items-center justify-center  '>
+    <div className='w-full h-full  flex flex-col sm:flex-row items-center justify-center  '>
       <img className='object-cover w-1/3' src='https://res.cloudinary.com/caraje/image/upload/v1682073684/AdivinaQue/brn9uwtb48nyf25r0bzc.webp' alt='Illustration popcorn' width gap-8={250} height={300} />
-      <div className='w-1/2 flex flex-col  items-center justify-center font-montserrat'>
-        <h2 className='font-extrabold text-4xl text-center text-adivinaGreen mb-4'>¡¡¡Ya has completado todos los niveles!!!</h2>
+      <div className='w-2/3 flex flex-col  items-center justify-center font-montserrat'>
+        <h2 className=' font-extrabold text-4xl text-center text-adivinaGreen mb-4'>¡¡¡Ya has completado todos los niveles!!!</h2>
         <h3 className=' text-base font-medium'>Vuelve mañana para poder ver nuevos niveles</h3>
         <h3 className=' text-base font-medium'>O participa en las otras categorias</h3>
       </div>
